a56046760a3aa3d74960a02a8756df2477f8dd80
rishabh kalra <rishabhkalra96@gmail.com>
2018-06-16 13:27:45 +0530

    part-4 complete, changing views into generic views for more easier access

--- polls/urls.py
+++ polls/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name='index_page'),
-    path('<int:question_id>/', views.detail, name='detail_view'),
-    path('<int:question_id>/results/', views.results, name='result_view'),
+    path('', views.IndexView.as_view(), name='index_page'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail_view'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='result_view'),
     path('<int:question_id>/vote/', views.vote, name='vote_view'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -2,45 +2,59 @@ from django.http import HttpResponse, Http404, HttpResponseNotFound, HttpRespons
 from django.shortcuts import render, get_object_or_404
 from django.template import loader
 from django.urls import reverse
+from django.views import generic
 
 from polls.models import Question, Choice
 
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def index(request):
-    print("Rendered index")
-    latest_question_list = Question.objects.order_by('-published_date')
+    def get_queryset(self):
+        """Return the last five published questions"""
+        return Question.objects.order_by('-published_date')[:5]
+
+#def index(request):
+#   print("Rendered index")
+#    latest_question_list = Question.objects.order_by('-published_date')
 # Required for creating a teplate render request
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return render(request, 'polls/index.html', context)
+#    template = loader.get_template('polls/index.html')
+#    context = {
+#        'latest_question_list': latest_question_list,
+#    }
+#    return render(request, 'polls/index.html', context)
 # return HttpResponse(template.render(context,request))
 
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
-def detail(request, question_id):
-    print("Rendered detail")
+
+#def detail(request, question_id):
+#    print("Rendered detail")
     #The below line is same as
-    question = get_object_or_404(Question, pk=question_id)
+#    question = get_object_or_404(Question, pk=question_id)
     #question = Question.objects.get(pk=question_id)
-    context = {'question': question}
-    return render(request, 'polls/detail.html',context )
+#    context = {'question': question}
+#    return render(request, 'polls/detail.html',context )
 # Another way to raise 404 errors
 #    question_text = get_object_or_404(Question, pk=question_id)
 #    return render(request, 'polls/detail.html', {'QUESTION_TEXT': question_text})
 #OR
 # return HttpResponse("You are looking at question %s." % question_id)
 
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    print("Rendered results")
-    #return HttpResponse(response % question_id)
-    return render(request, 'polls/results_view', {'question': question})
+# def results(request, question_id):
+#    question = get_object_or_404(Question, pk=question_id)
+#    print("Rendered results")
+#    #return HttpResponse(response % question_id)
+#    return render(request, 'polls/results_view', {'question': question})
 
 
 def vote(request, question_id):
-
     print("Rendered vote")
     question = get_object_or_404(Question, pk=question_id)
     try:
@@ -48,7 +62,7 @@ def vote(request, question_id):
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         #Redisplay the question voting form
-        context = {'question': question,'error_message': "You did not select a choice" ,}
+        context = {'question': question,'error_message': "You did not select a choice" , }
         return render(request, 'polls/detail.html', context)
 
     else:
