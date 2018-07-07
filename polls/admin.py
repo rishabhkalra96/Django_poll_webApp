31b02f1d1e302a5469745a6ebc2c496990815ccb
rishabh kalra <rishabhkalra96@gmail.com>
2018-07-07 15:38:05 +0530

    more changes to django admin

--- polls/admin.py
+++ polls/admin.py
@@ -2,14 +2,21 @@ from django.contrib import admin
 from .models import Question, Choice
 
 # This is done to make our polls app accessible in the admin page
-class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'published_date', 'published_recently')
+
     fieldsets = [
         ('Question Information',{ 'fields': ['question_text']} ),
         ('Date Information', {'fields' : ['published_date'], 'classes': ['collapse']}),
     ]
     inlines = [ChoiceInline]
+
+    # To add a filter column in admin page which will be based upon date_published, the type of filter options
+    # to display is automatically managed by django, try 'question_text' instead of 'published_date' below
+
+    list_filter = ['published_date']
 
 admin.site.register(Question,QuestionAdmin)
