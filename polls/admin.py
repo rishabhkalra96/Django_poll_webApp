b3fc8dc95a27026b6e5dc8254281e69eeb613e0a
rishabh kalra <rishabhkalra96@gmail.com>
2018-07-07 15:14:04 +0530

    managing the admin page

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 
 # This is done to make our polls app accessible in the admin page
-admin.site.register(Question)
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Question Information',{ 'fields': ['question_text']} ),
+        ('Date Information', {'fields' : ['published_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question,QuestionAdmin)
